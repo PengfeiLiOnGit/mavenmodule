f6b7ebb51dde4cd2cb3e13758961ff41c73a34f8
LIPENGFEI <jonyonline@sina.com>
2020-03-29 15:20:31 +0800

    index

--- web/src/main/java/com/jony/web/sys/SysUserController.java
+++ web/src/main/java/com/jony/web/sys/SysUserController.java
@@ -1,14 +1,27 @@
 package com.jony.web.sys;
 
+import com.jony.dao.sys.entity.SysUser;
+import com.jony.service.sys.ISysUser;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class SysUserController {
+    @Autowired
+    private ISysUser sysUser;
 
-    @RequestMapping({"","/index"})
+    @RequestMapping({"/index"})
     public String index(){
+        List<SysUser> users = sysUser.selectAll();
         System.out.println("hello");
         return "index";
+    }
+
+    @RequestMapping({"/"})
+    public String ii(){
+        return "tt";
     }
 }
